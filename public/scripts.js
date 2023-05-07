fd9531e08a5bec7b886a1197f04c31a7049446f5
Caitlin Lewis <69729525+clewis7@users.noreply.github.com>
2023-05-07 15:59:49 -0400

    ready to send frontend to backend

--- public/scripts.js
+++ public/scripts.js
@@ -11,8 +11,6 @@ function helpNav() {
     reset();
     document.getElementById("home").className = "inactive";
     document.getElementById("disp").className = "inactive";
-    document.getElementById("opponent").className = "inactive";
-    document.getElementById("disp2").className = "inactive";
     document.getElementById("help").className = "active";
 }
 
@@ -21,30 +19,37 @@ function reset() {
     document.getElementById("home").className = "active";
     document.getElementById("opponent").className = "inactive";
     document.getElementById("disp2").className = "inactive";
+    document.getElementById("opponent-options-rps").className = "inactive";
+    document.getElementById("opponent-options-rpsls").className = "inactive";
     var ele = document.getElementsByName("option");
     for(var i=0;i<ele.length;i++)
         ele[i].checked = false;
     var ele = document.getElementsByName("opp");
         for(var i=0;i<ele.length;i++)
             ele[i].checked = false;
+    var ele = document.getElementsByName("opponent-options-rps");
+        for(var i=0;i<ele.length;i++)
+            ele[i].checked = false;
+    var ele2 = document.getElementsByName("opponent-options-rpsls");
+        for(var i=0;i<ele2.length;i++)
+            ele2[i].checked = false;
     document.getElementById("disp2").innerHTML
             = "";
     document.getElementById("disp").innerHTML
             = "";
 }
 
-let game = None;
-let opponent = None;
-
 function display() {
     if (document.getElementById("rps").checked) {
         document.getElementById("disp").innerHTML = "Would you like a random draw or to play an opponent?"
         document.getElementById("opponent").className = "active";
         document.getElementById("disp2").className = "active";
+        document.getElementById("game").innerHTML = "rps";
     } else if (document.getElementById("rpsls").checked) {
         document.getElementById("disp").innerHTML = "Would you like a random draw or to play an opponent?"
         document.getElementById("opponent").className = "active";
         document.getElementById("disp2").className = "active";
+        document.getElementById("game").innerHTML = "rpsls";
     } else {
         document.getElementById("disp").innerHTML
                     = "You have not selected which game you would like to play.";
@@ -53,10 +58,26 @@ function display() {
 
 function getGame() {
     if (document.getElementById("opps").checked) {
-        document.getElementById("disp2").innerHTML = "You have selected to play {} against {}.";
+        document.getElementById("disp2").innerHTML = "Please select what you would like to play below.";
+        if (document.getElementById("game").innerHTML == "rps") {
+            document.getElementById("opponent-options-rpsls").className = "inactive";
+            document.getElementById("opponent-options-rps").className = "active";
+        } else {
+            document.getElementById("opponent-options-rps").className = "inactive";
+            document.getElementById("opponent-options-rpsls").className = "active";
+        }
     } else if (document.getElementById("no-opponent").checked) {
-        document.getElementById("disp2").innerHTML = "You have selected to play {} against {}.";
+        document.getElementById("disp2").innerHTML = "You have selected to play a random opponent.";
+        playRandom();
     } else {
         document.getElementById("disp2").innerHTML = "You have not selected an opponent style.";
 }
+}
+
+function playRandom() {
+
+}
+
+function playOpp() {
+
 }
