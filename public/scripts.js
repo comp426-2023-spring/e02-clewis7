005054581481038fb3d5156a35bec91ad9d4f3c8
Caitlin Lewis <69729525+clewis7@users.noreply.github.com>
2023-05-07 18:58:09 -0400

    finally done

--- public/scripts.js
+++ public/scripts.js
@@ -27,16 +27,22 @@ function reset() {
     var ele = document.getElementsByName("opp");
         for(var i=0;i<ele.length;i++)
             ele[i].checked = false;
-    var ele = document.getElementsByName("opponent-options-rps");
+    var ele = document.getElementsByName("player");
         for(var i=0;i<ele.length;i++)
             ele[i].checked = false;
-    var ele2 = document.getElementsByName("opponent-options-rpsls");
+    var ele2 = document.getElementsByName("player2");
         for(var i=0;i<ele2.length;i++)
             ele2[i].checked = false;
     document.getElementById("disp2").innerHTML
             = "";
     document.getElementById("disp").innerHTML
             = "";
+    document.getElementById("result2").className = "inactive"
+    document.getElementById("result").className = "inactive"
+    document.getElementById("result3").className = "inactive"
+    document.getElementById("result4").className = "inactive"
+    document.getElementById("result5").className = "inactive"
+    document.getElementById("result6").className = "inactive"
 }
 
 function display() {
@@ -58,7 +64,7 @@ function display() {
 
 function getGame() {
     if (document.getElementById("opps").checked) {
-        document.getElementById("disp2").innerHTML = "Please select what you would like to play below.";
+        document.getElementById("disp2").innerHTML = "Please select what you would like to play below:";
         if (document.getElementById("game").innerHTML == "rps") {
             document.getElementById("opponent-options-rpsls").className = "inactive";
             document.getElementById("opponent-options-rps").className = "active";
@@ -67,6 +73,8 @@ function getGame() {
             document.getElementById("opponent-options-rpsls").className = "active";
         }
     } else if (document.getElementById("no-opponent").checked) {
+        document.getElementById("opponent-options-rps").className = "inactive";
+        document.getElementById("opponent-options-rpsls").className = "inactive";
         document.getElementById("disp2").innerHTML = "You have selected to play a random opponent.";
         playRandom();
     } else {
@@ -74,10 +82,66 @@ function getGame() {
 }
 }
 
-function playRandom() {
+async function playRandom() {
+    document.getElementById("result").className = "active";
+
+    let game = document.getElementById("game").innerHTML
+    let baseurl = window.location.href + 'app/';
+    let url = baseurl + game + '/';
+
+    let response = await fetch(url);
+    let result = await response.json();
+    console.log(result)
+
+    document.getElementById("result2").className = "active"
+    result = result['player']
+    document.getElementById("result2").innerHTML = "<img src='/img/" +result+ ".jpg'>";
+    
 
 }
 
-function playOpp() {
+function getOpp() {
+    if (document.getElementById("rock").checked) {
+        document.getElementById("shot").innerHTML = "rock";
+        playOpp();
+    } else if (document.getElementById("paper").checked) {
+        document.getElementById("shot").innerHTML = "paper";
+        playOpp();
+    } else if (document.getElementById("scissors").checked) {
+        document.getElementById("shot").innerHTML = "scissors";
+        playOpp();
+    } else if (document.getElementById("lizard").checked) {
+        document.getElementById("shot").innerHTML = "lizard";
+        playOpp();
+    } else {
+        document.getElementById("shot").innerHTML = "spock";
+        playOpp();
+    }
+    
+}   
+
+async function playOpp() {
+    document.getElementById("result").className = "active";
+
+    let game = document.getElementById("game").innerHTML
+    let opp = document.getElementById("shot").innerHTML
+    let baseurl = window.location.href + 'app/';
+    let url = baseurl + game + '/play/' + opp + '/';
+
+    let response = await fetch(url);
+    let result = await response.json();
+    console.log(result)
+
+    document.getElementById("result2").className = "active"
+    document.getElementById("result5").className = "active"
+    document.getElementById("result5").innerHTML = result['result'].toUpperCase();
+    document.getElementById("result6").className = "active"
+    player = result['player']
+    document.getElementById("result2").innerHTML = "<img src='/img/" +player+ ".jpg'>";
+    document.getElementById("result3").className = "active"
+    document.getElementById("result4").className = "active"
+    opponent = result['opponent']
+    document.getElementById("result4").innerHTML = "<img src='/img/" +opponent+ ".jpg'>";
+    
 
 }
