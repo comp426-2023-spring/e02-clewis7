07ab49330f8635418204bb5432a4fe03c4be947e
Caitlin Lewis <69729525+clewis7@users.noreply.github.com>
2023-05-07 11:24:06 -0400

    getting game options ready

--- public/scripts.js
+++ public/scripts.js
@@ -5,9 +5,44 @@
 function homeNav() {
     document.getElementById("help").className = "inactive";
     document.getElementById("home").className = "active";
+    document.getElementById("disp").className = "active";
 }
 
 function helpNav() {
     document.getElementById("home").className = "inactive";
+    document.getElementById("disp").className = "inactive";
+    document.getElementById("opponent").className = "inactive";
+    document.getElementById("disp2").className = "inactive";
     document.getElementById("help").className = "active";
+}
+
+function display() {
+    if (document.getElementById("rps").checked) {
+        var game = "rps";
+        document.getElementById("disp").innerHTML = "Would you like a random draw or to play an opponent?"
+        document.getElementById("opponent").className = "active";
+        document.getElementById("disp2").className = "active";
+    } else if (document.getElementById("rpsls").checked) {
+        var game = "rpsls";
+        document.getElementById("disp").innerHTML = "Would you like a random draw or to play an opponent?"
+        document.getElementById("opponent").className = "active";
+        document.getElementById("disp2").className = "active";
+    } else {
+        document.getElementById("disp").innerHTML
+                    = "You have not selected which game you would like to play.";
+    }
+}
+
+function getGame() {
+    var opponent = None;
+    if (document.getElementById("opponent").checked) {
+        opponent = True;
+        document.getElementById("disp2").innerHTML = "You have selected to play {} against {}.";
+} else if (document.getElementById("no-opponent").checked) {
+        opponent = False; 
+        document.getElementById("disp2").innerHTML = "You have selected to play {} against {}.";
+} else {
+    document.getElementById("disp2").innerHTML
+    = "You have not selected an opponent style.";
+}
 }
