2d4f9bf7f3bcdffe74cbfd342efc5c84d8e647e5
Caitlin Lewis <69729525+clewis7@users.noreply.github.com>
2023-05-07 10:29:55 -0400

    progress with help page

--- public/scripts.js
+++ public/scripts.js
@@ -2,3 +2,12 @@
 // check out the coin-server example from a previous COMP 426 semester.
 // https://github.com/jdmar3/coinserver
 
+function homeNav() {
+    document.getElementById("help").className = "inactive";
+    document.getElementById("home").className = "active";
+}
+
+function helpNav() {
+    document.getElementById("home").className = "inactive";
+    document.getElementById("help").className = "active";
+}
